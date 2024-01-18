7d7f075de3ef99a3f860e0d7d38ef08350f593db
cab <caballero2691@gmail.com>
2024-01-18 15:30:18 +0100

    Match with Soubestre

--- swcorr.py
+++ swcorr.py
@@ -63,42 +63,37 @@ class swcorr(object):
             self.nclust[iclus]['idx_clus'] = idx_clus
             self.nclust[iclus]['sw_tem'] = np.mean(self.SW[:,idx_clus],axis=1)
             # print(max_stack)
-            #Iterative step 2 of Soubestre et al. 2018
+
+        #Iterative step 2 of Soubestre et al. 2018
+        cc_whole = copy.deepcopy(self.CC)
+        for iclus in np.arange(self.NumClusters):    
+            CC_cluster = []
+            #Stacking over the seleceted windows
             idx_iter = copy.deepcopy(self.nclust[iclus]['idx_clus'])
-            # Loop for the central point
-            for iter in np.arange(4):
-                cc_whole = copy.deepcopy(self.CC)
-                CC_cluster = []
-                #Stacking over the seleceted windows
-                for count, idxCC in enumerate(idx_iter):
-                    CC_cluster.append(np.sum(cc_whole[idxCC,idx_iter]))
-                CC_cluster = np.array(CC_cluster)
-                max_tmp = np.argmax(CC_cluster)
-                max_idx = idx_iter[max_tmp]
-                #print(max_idx)
-                idx_tmp = np.where(cc_whole[max_idx,:]>self.CC_thres)[0]
-                idx_iter = copy.copy(idx_tmp)
+            for count, idxCC in enumerate(idx_iter):
+                CC_cluster.append(np.sum(cc_whole[idxCC,idx_iter])) 
+            CC_cluster = np.array(CC_cluster)
+            max_tmp = np.argmax(CC_cluster)
+            max_idx = idx_iter[max_tmp]            
             self.nclust[iclus]['maxfinal'] = max_idx
             self.nclust[iclus]['idx_final']=[]
             central_idx.append(max_idx)
-        # print(central_idx)
-        for iclus in np.arange(self.NumClusters):
-            cc_final = copy.deepcopy(self.CC)
-            for idx in self.nclust[iclus]['idx_clus']:
-                tmp = cc_final[idx,central_idx]
-                cluster_final = np.argmax(tmp)
-                self.nclust[cluster_final]['idx_final'].append(idx)
 
-        #Conver to array, sort the index, and calculate the spectrla \
+        for iday in np.arange(self.CC.shape[1]):
+            tmp = cc_whole[iday,central_idx]
+            cluster_final = np.argmax(tmp)
+            if cc_whole[iday,central_idx[cluster_final]] >self.CC_thres:
+                self.nclust[cluster_final]['idx_final'].append(iday)
+
+        #Conver to array, sort the index, and calculate the spectral \
         # width template
         erase_list = []
         for iclus in np.arange(self.NumClusters):
             self.nclust[iclus]['idx_final'] = np.sort(np.array(self.nclust[iclus]['idx_final']))
-            if self.nclust[iclus]['idx_final'].size:
+            if  self.nclust[iclus]['idx_final'].size:
                 self.nclust[iclus]['SW_final'] = np.mean(self.SW[:,self.nclust[iclus]['idx_final']],axis=1)
             else:
-                print('Not elemennts in cluster: ' + str(iclus) + ' because similarity' )
-                # print('changing for initial cluster samples')
+                print('Not elements in cluster: ' + str(iclus) + ' because similarity' )
                 print('Deleting')
                 self.nclust[iclus]['SW_final'] = self.nclust[iclus]['sw_tem']
                 erase_list.append(iclus)
@@ -106,6 +101,34 @@ class swcorr(object):
             for ic in erase_list:
                 del self.nclust[ic]
                 self.NumClusters -=1
+
+        clust_tmp= {}
+        date_tmp = []
+        for iclus in np.arange(self.NumClusters):
+            clust_tmp[iclus] = {}
+            clust_tmp[iclus]['idx']= copy.deepcopy(self.nclust[iclus]['idx_final'])
+            date_tmp.append([])
+        for iter in np.arange(4):
+            central_tmp = []
+            for iclus in np.arange(self.NumClusters):
+                CC_cluster = []
+                #Stacking over the seleceted windows
+                idx_iter = clust_tmp[iclus]['idx']
+                for count, idxCC in enumerate(idx_iter):
+                    CC_cluster.append(np.sum(cc_whole[idxCC,idx_iter]))
+                CC_cluster = np.array(CC_cluster)
+                max_tmp = np.argmax(CC_cluster)
+                max_idx = idx_iter[max_tmp]
+                central_tmp.append(max_idx)
+                #print(max_idx)
+            for iday in np.arange(self.CC.shape[1]):
+                tmp = cc_whole[iday,central_tmp]
+                cluster_final = np.argmax(tmp)
+                #if cc_whole[iday,central_tmp[cluster_final]] >self.CC_thres:
+                date_tmp[cluster_final].append(iday)            
+            for iclus in np.arange(self.NumClusters):                
+                self.nclust[iclus]['idx'] = np.array(date_tmp[iclus])
+            
         return
 
     
